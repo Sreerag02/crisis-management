58a59d7360b8a95d51c1b1e82b7b24ecddd64b18
Sreerag02 <157096211+Sreerag02@users.noreply.github.com>
2026-04-18 17:51:40 +0530

    Update API_URL to point to the new server

--- crisis-client/src/services/api.js
+++ crisis-client/src/services/api.js
@@ -1,4 +1,4 @@
-const API_URL ='https://crisis-management-web.vercel.app/';
+const API_URL ='https://crisis-management-server.vercel.app/';
 //committed
 
 const handleResponse = async (response) => {
